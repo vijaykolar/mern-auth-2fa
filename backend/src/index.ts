78a2535a834c30848d755080865e0a294200ca09
vijaykolar <shreerakshaupase1993@gmail.com>
2025-01-11 00:57:43 +0530

    Add environment configuration and improve server setup
    - Updated .gitignore to include all .env files
    - Integrated dotenv for environment variable management
    - Enhanced server configuration with dynamic port and environment mode
    - Added cookie-parser and CORS middleware for improved request handling
    - Created utility function to retrieve environment variables
    - Established app configuration with default values

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,35 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { config } from "./config/app.config";
+
+const BASE_PATH = config.BASE_PATH;
+
+dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: config.APP_ORIGIN,
+    credentials: true,
+  })
+);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!@@@@");
+  res.send("Hello World!!!");
 });
 
 app.get("/app", (req, res) => {
   res.send("Hello World app");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(config.PORT, () => {
+  console.log(
+    `Server is running on http://localhost:${config.PORT} in ${config.NODE_ENV} mode`
+  );
 });
